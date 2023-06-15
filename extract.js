0e346ac524baef11cb2e4359192a82b361f1f2ae
amber <a06000208@protonmail.com>
2023-06-15 06:26:21 -0500

    Try working around an EACCES error

--- extract.js
+++ extract.js
@@ -1,17 +1,27 @@
 // Simple code to extract data downloaded by the npm script
 // Would have used something on the cli but couldn't find anything decent
 
-import { join } from "path";
+import { join } from "node:path";
+import { platform } from "node:os";
+import { chmod } from "node:fs/promises";
 import { pathExists } from "fs-extra";
 import sevenBin from "7zip-bin";
+import Seven from "node-7z";
 import { directory } from "./modules/constants.js";
-import Seven from "node-7z"; // Doesn't have named exports
 
 const cacheDirectory = join(directory, "cache");
 const archivePath = join(directory, "cache", "fortnite_pages_current.xml.7z");
 
 const pathTo7zip = sevenBin.path7za;
 console.log("7z binary:", pathTo7zip);
+
+if (platform() == "linux") {
+    try {
+        await chmod(pathTo7zip, "755");
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 if (await pathExists(archivePath)) {
     console.log("found fortnite_pages_current.xml.7z, attempting to extract...");
